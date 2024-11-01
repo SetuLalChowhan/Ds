4179d19adf59fe4b66f99ba93a84f4a1b195320a
setu <setulalchowhan@gmail.com>
2024-11-01 13:03:00 +0600

    numpy array

--- Array.py
+++ Array.py
@@ -1,80 +1,79 @@
-import ctypes
-class MyList:
-    def __init__(self):
-        self.size =1
-        self.n=0
-        self.A= self.__make_array(self.size)
-    
-    def __make_array(self,capacity):
-        #ctype
-        return (capacity*ctypes.py_object)()
-    def __len__(self):
-        return self.n
-    def __str__(self):
-        result =''
-        for i in range(self.n):
-            result =result + str(self.A[i])+"," # 12,14,
-        
-        return "["+result[:-1]+"]"
+# import ctypes
+# class MyList:
+#     def __init__(self):
+#         self.size =1
+#         self.n=0
+#         self.A= self.__make_array(self.size)
+#     def __make_array(self,capacity):
+#         #ctype
+#         return (capacity*ctypes.py_object)()
+#     def __len__(self):
+#         return self.n
+#     def __str__(self):
+#         result =''
+#         for i in range(self.n):
+#             result =result + str(self.A[i])+"," 
+#         return "["+result[:-1]+"]"
+#     def append(self,item):
+#         if self.size == self.n:
+#             #call
+#             self.__resize(self.size*2)
+#         self.A[self.n]=item
+#         self.n+=1
+#     def insert(self,pos,item):
+#         if self.size ==self.n:
+#             self.__resize(2*self.size)
+#         for i in range(self.n-1,pos-1,-1):
+#             self.A[i+1] =self.A[i]    
+#         self.A[pos] =item
+#         self.n=self.n+1
+#     def delete(self,pos):
+#         for i in range(pos,self.n-1):
+#             self.A[i]=self.A[i+1]
+#         self.n=self.n-1
+#     def find(self,item):
+#         for i in range(self.n):
+#             if self.A[i] == item:
+#                 return i
+#         return "Value Error"  
+#     def pop(self):
+#         if self.n==0:
+#             return "empty list"
+#         self.n-=1
+#         print(self.A[self.n-1])
+#     def clear(self):
+#         self.n=0
+#         self.size=1
+#     def __resize(self,new_capacity):
+#         #create new array with double capacity
+#        B = self.__make_array(new_capacity)
+#        self.size =new_capacity
+#        #copy the content of A to B
+#        for i in range(self.n):
+#            B[i] =self.A[i]
+#        #reassign
+#        self.A=B
 
-    def append(self,item):
-        if self.size == self.n:
-            #call
-            self.__resize(self.size*2)
-        self.A[self.n]=item
-        self.n+=1
-    
-    def insert(self,pos,item):
-        if self.size ==self.n:
-            self.__resize(2*self.size)
-        
-        for i in range(self.n-1,pos-1,-1):
-            self.A[i+1] =self.A[i]
-        
-        self.A[pos] =item
-        self.n=self.n+1
-    
-    def delete(self,pos):
-        for i in range(pos,self.n-1):
-            self.A[i]=self.A[i+1]
-        
-        self.n=self.n-1
-
-    def find(self,item):
-        for i in range(self.n):
-            if self.A[i] == item:
-                return i
-        
-        return "Value Error"  
-    def pop(self):
-        if self.n==0:
-            return "empty list"
-        self.n-=1
-        print(self.A[self.n-1])
-    def clear(self):
-        self.n=0
-        self.size=1
-    def __resize(self,new_capacity):
-        #create new array with double capacity
-       B = self.__make_array(new_capacity)
-       self.size =new_capacity
-       #copy the content of A to B
-       for i in range(self.n):
-           B[i] =self.A[i]
-       #reassign
-       self.A=B
-
-L=MyList()
-L.append(12)
-L.append(14)
-L.append(17)
-L.append(11)
+# L=MyList()
+# L.append(12)
+# L.append(14)
+# L.append(17)
+# L.append(11)
 # L.clear()
 # L.pop()
-print(L)
-L.delete(1)
-print(L)
+# print(L)
+# L.delete(1)
+# print(L)
 # L.insert(2,20)
 # print(L)
+
+import calendar
+import datetime
+
+year= datetime.datetime.now().year
+
+print(calendar.calendar(year))
+
+
 
 
---
+++ numpyArray.py
@@ -0,0 +1,86 @@
+import numpy as np  #nd array create
+
+#create array
+# l1=[10,20,30,40,11,1,2,3,4]
+# l2=[[2,3,4],[22,21,22],[1,2,3],[31,32,33]]
+# array1 = np.array(l1)
+# array2 = np.array(l2,dtype=int)
+# array3 = np.arange(1,8)
+# array4 = np.arange(11,17).reshape((3,2))
+# array5 =np.zeros(4,dtype=int)
+# array6 =np.ones(4,dtype=int)
+# print(array6)
+
+#attribute of Numpy
+# 1.ndim
+# 2.shape
+# 3.size
+# 4.dtype
+# 5.itemsize
+
+# print(array2.ndim)
+
+# print(array2.shape)
+
+# print(array1.size)
+
+# print(array1.dtype)
+# print(array1.itemsize)
+
+#indexing in Numpy Array
+
+# print(array1[3])
+# print(array2[1,2])
+# print(array2[0,:]) row and colume
+# print(array2[:,1])
+
+#slicing
+# 1d [start,stop,step]
+# print(array1[1:3]) 
+# print(array1[1:6:2])\
+# print(array1[-1:-3:-1])
+# print(array1[::2])
+# print(array1[::-1])
+# 2d [[rowstart:rowend],[columestart:columeend]]
+# print(array2)
+# print(array2[1,])
+# print(array2[:,1])
+# print(array2[1:3,1:3])
+# print(array2[1:3,])
+# print(array2[:,1:3])
+# print(array2[1:3,1])
+
+#arithmatic operations on Numpy Arrays
+# 1.Addition(+)
+# 2.Subtraction(-)
+# 3.Multiplication(*)
+# 4.Matrix Multipliccation(@)
+# 5.Division(/)
+# 6.Floor Division(//)
+# 7.Exponentiation(**)
+# 8.Transpose
+
+# x = np.array([[1,2],[3,4]])
+# y = np.array([[11,12],[13,14]])
+
+# z= x+y
+# z= x-y
+# z=x*y
+# z =x@y
+# z=y/x
+# z=y//x
+# z=y**x
+# z=y%x
+# print(z)
+# print(x.transpose())
+
+# sorting 2d array
+
+# x =np.array([[12,11,15],[21,25,20],[18,27,16]])
+
+# y= np.sort(x)
+# y= np.sort(x,axis=1)
+# y= np.sort(x,axis=0)
+# y = x.argsort(x,axis=1)
+# y = x.argsort(x,axis=0)
+# print(y)
